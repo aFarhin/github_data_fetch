ac6af86ba19bd5e6e37d44784a09bbd97aae5c4c
aFarhin <ahmeda.farhin786@gmail.com>
2023-06-07 13:45:40 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [searchUsername, setSearchUsername] = useState('');
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = async () => {
+    if (searchUsername.trim() !== '') {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`https://api.github.com/users/${searchUsername}`);
+        const data = await response.json();
+        console.log(data)
+        setUser(data);
+       
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div className='main'>
+    <div className='search'>
+      <input
+        type="text"
+        value={searchUsername}
+        onChange={(e) => setSearchUsername(e.target.value)}
+        placeholder="Enter GitHub username"
+      />
+      <button onClick={handleSearch}>Search</button>
+      </div>
+      {isLoading ? (
+        <div className="loader-container">
+        <div className="loader"></div>
+        <span>Loading...</span>
+      </div>
+      ) : (
+        user && (
+          <div className='container'>
+  <img src={user.avatar_url} alt={user.login} />
+  <p><strong> User Name: </strong>  {user.login}</p>
+  <p><strong>Name: </strong> {user.name}</p>
+  <p><strong>Bio: </strong> {user.bio}</p>
+  <p><strong>Followers: </strong> {user.followers}</p>
+  <p><strong>Following: </strong> {user.following}</p>
+  <p><strong>Public Repositories: </strong> {user.public_repos}</p>
+</div>
+
+        )
+      )}
     </div>
   );
-}
+  
+};
 
 export default App;
